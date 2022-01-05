334e1a100a3fadd8b429ca8304480e1471202ac5
Marcelo Bettini <marcelobettini@MacBook-Pro-de-Marcelo.local>
2022-01-05 15:02:47 -0300

    secret re render with username

--- routes/secret.js
+++ routes/secret.js
@@ -1,7 +1,8 @@
 "use strict";
 const express = require("express");
 const router = express.Router();
+
 router.get("/", (req, res) => {
-  res.render("secret");
+  res.render("secret", {user: req.session.user});
 });
 module.exports = router;
